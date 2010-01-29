533f76b2c58beb51f4ddb3053cda0eeb37eb67a8
pwilliams <pwilliams@502fd8d8-047e-0410-9e1b-94570f3051e5>
2010-01-29 20:44:15 +0000

    improved load config files for rails
    git-svn-id: http://svn.openlogic.local/samlsp/trunk@14 502fd8d8-047e-0410-9e1b-94570f3051e5

--- rails/init.rb
+++ rails/init.rb
@@ -1,11 +1,5 @@
 SamlSp.logger = Rails.logger
 
-["config/saml_sp.conf", "config/#{Rails.env}/saml_sp.conf"].each do |f|
-  config_file = File.join(Rails.root, f)
-  if File.exists? config_file
-    SamlSp::Config.load_file config_file
-  else
-    SamlSp.logger.debug "saml-sp: Skipping config file '#{config_file}' because it does not exist"
-  end
-end
+config_file = File.join(Rails.root, 'config/saml_sp.conf')
+SamlSp::Config.load_file config_file if File.exists? config_file
 
